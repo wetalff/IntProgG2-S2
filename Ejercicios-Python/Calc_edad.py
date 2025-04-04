31faba60a04bf4ff740cdff6e5b3941bacc1cd3e
wetalff <alfredito.osejo@gmail.com>
2025-04-03 20:13:50 -0600

    Ejercicios en python de la semana 2

--- Ejercicios-Python/Calc_edad.py
+++ Ejercicios-Python/Calc_edad.py
@@ -1,4 +1,6 @@
 Años_actual = 2025
 Año_Nac = int(input("Dime el año en que naciste:"))
 Edad = Años_actual-Año_Nac
-print("Tu edad actual es: ", Edad)
+print("Tu edad actual es: ", Edad, "años")
+      
+      
